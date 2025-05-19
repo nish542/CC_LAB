0f4fa69154d9e754eeaf7256458b669b9a2cfb6b
Jai Gupta <jaiwork04@gmail.com>
2025-05-19 09:35:05 +0530

    final

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const soap = require('strong-soap').soap;
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
